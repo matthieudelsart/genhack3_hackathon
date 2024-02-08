dd266bcfecd8e7bd73d427e86ae85ae2d45169e9
FrancoisLbrn <francois.lebrun3@gmail.com>
2024-02-08 16:30:55 +0100

    Modify main.py adding arguement scenario to generative_model

--- main.py
+++ main.py
@@ -7,9 +7,10 @@ import numpy as np
 import logging
 
 
-logging.basicConfig(filename="check.log", level=logging.DEBUG, 
-                    format="%(asctime)s:%(levelname)s: %(message)s", 
+logging.basicConfig(filename="check.log", level=logging.DEBUG,
+                    format="%(asctime)s:%(levelname)s: %(message)s",
                     filemode='w')
+
 
 def simulate(noise, scenario):
     """
@@ -29,27 +30,27 @@ def simulate(noise, scenario):
     """
 
     try:
-        output = generative_model(noise)
-        message = "Successful simulation" 
-        assert output.shape == (noise.shape[0], 4), "Shape error, it must be (noise.shape[0], 4). Please verify the shape of the output."
-        
+        output = generative_model(noise, scenario)
+        message = "Successful simulation"
+        assert output.shape == (
+            noise.shape[0], 4), "Shape error, it must be (noise.shape[0], 4). Please verify the shape of the output."
+
         # write the output
         np.save("output.npy", output)
 
     except Exception as e:
         message = e
-                
+
     finally:
         logging.debug(message)
 
     return output
 
-    
+
 if __name__ == "__main__":
     SCENARIO_NUMBER = 1  # <- PICK A SCENARIO NUMBER BETWEEN 1 AND 9
     noise = np.load("data/noise.npy")
-    scenario = np.zeros((noise.shape[0], 9))  # create a vector of zeros with shape (n_samples, 9)
+    # create a vector of zeros with shape (n_samples, 9)
+    scenario = np.zeros((noise.shape[0], 9))
     scenario[:, SCENARIO_NUMBER-1] = 1
     simulate(noise, scenario)
-    
-    
